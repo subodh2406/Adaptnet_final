d0c821006c627d4af2e2dd454f7c3c6cf6d65135
subodh2406 <158086565+subodh2406@users.noreply.github.com>
2025-01-28 19:23:50 +0530

    updated

--- render_app.py
+++ render_app.py
@@ -54,7 +54,7 @@ import sys
 from threading import Thread
 
 # Import model_utils (works directly as it's in the same directory)
-from model_utils import load_all_models, preprocess_input
+from app.model_utils import load_all_models, preprocess_input
 
 # Get port from environment variable
 PORT = int(os.environ.get("PORT", 10000))
